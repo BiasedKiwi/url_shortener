a28ed353512678d4bd6206596f87134620ab7451
BiasedKiwi <61089773+BiasedKiwi@users.noreply.github.com>
2024-02-20 08:53:35 +0100

    things

--- app/page.js
+++ app/page.js
@@ -13,8 +13,8 @@ import shorten from "@/app/lib/shortener";
 export default function Home() {
   return (
     <>
-      <nav className="max-w-7xl m-auto my-10 flex justify-between items-center antialiased">
-        <div className="flex gap-12 items-center text-gray">
+      <nav className="max-w-7xl m-auto my-10 flex justify-between items-center antialiased max-md:mx-6">
+        <div className="flex gap-12 items-center text-gray relative">
           <Link href="/">
             <Image
               src="/logo.svg"
@@ -23,7 +23,7 @@ export default function Home() {
               height={130}
             />
           </Link>
-          <div className="flex gap-8">
+          <div className="flex gap-8 max-md:hidden">
             <Link href="/features" className="hover:text-black duration-100">
               Features
             </Link>
@@ -35,7 +35,7 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        <div className="flex gap-10 items-center text-gray">
+        <div className="flex gap-10 items-center text-gray max-md:hidden">
           <Link href="login" className="hover:text-black duration-100">
             Login
           </Link>
@@ -46,46 +46,82 @@ export default function Home() {
             Sign Up
           </button>
         </div>
+        {/* Mobile Nav */}
+        <div id="hamburgerMenu">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-10 h-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+        <div className="hidden" id="mobileNav">
+          <div className="max-md:bg-dark_violet text-white font-bold text-lg gap-5 rounded-lg flex flex-col items-center justify-between absolute px-10 py-10 top-28 mx-5 left-0 right-0">
+            <div className="flex flex-col gap-7 items-center">
+              <Link href="/features">Features</Link>
+              <Link href="/pricing">Pricing</Link>
+              <Link href="/resources">Resources</Link>
+            </div>
+            <div className="bg-slate-500 w-full h-[1px]" />
+            <div className="flex flex-col items-center gap-7">
+              <Link href="/login">Login</Link>
+              <button
+                onClick={() => console.log("Clicked")}
+                className="bg-cyan py-3 px-6 rounded-full text-white font-bold"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
       </nav>
-      <main className="flex flex-row max-w-7xl m-auto justify-between">
+      <main className="flex flex-row max-lg:flex-col-reverse max-w-7xl m-auto justify-between max-md:text-center">
         <div>
-          <h1 className="text-7xl font-bold mt-28 leading-tight antialiased">
+          <h1 className="text-7xl font-bold mt-28 leading-tight antialiased max-md:text-5xl max-md:mt-12">
             More than just
             <br />
             shorter links
           </h1>
-          <p className="text-gray leading-8 mt-2 text-xl">
-            Build your brand&apos;s recognition and get detailed
-            <br /> insights on how your links are performing.
+          <p className="text-gray leading-8 mt-2 text-xl max-md:mx-6 max-md:mt-5">
+            Build your brand&apos;s recognition and get detailed insights on how
+            your links are performing.
           </p>
-          <button className="bg-cyan py-4 px-10 rounded-full text-white font-bold mt-8">
+          <button className="bg-cyan py-4 px-10 rounded-full text-white font-bold mt-8 max-md:py-5 max-md:text-xl">
             Get Started
           </button>
         </div>
         <Image
           src="/illustration-working.svg"
-          width={800}
+          width={100}
           height={100}
           alt="Person working"
-          className="absolute right-0 mt-9"
+          className="right-0 mt-9 w-full max-md:static max-md:w-full max-md:h-full"
         />
       </main>
-      <div className="max-w-7xl m-auto mb-[-50px] z-10 mt-40 p-12 rounded-lg bg-dark_violet flex relative">
+      <div className="max-w-7xl m-auto mb-[-50px] z-10 mt-40 p-12 rounded-lg bg-dark_violet flex relative max-md:h-100 max-md:justify-between max-md:mx-5 max-md:p-6 max-md:mt-20">
         <Image
           src="/bg-shorten-desktop.svg"
           alt=""
-          width={1000}
-          height={1000}
-          className="absolute top-0 left-0 z-0 bg-cover w-full h-full"
+          width={100}
+          height={100}
+          className="absolute top-0 left-0 z-0 w-full h-full"
         />
-        <div className="flex gap-5 justify-between">
+        <div className="flex gap-5 justify-between max-md:flex-col max-md:m-auto">
           <input
             placeholder="Shorten a link here..."
-            className="rounded-md grow-[3] h-full pl-4 z-10"
+            className="rounded-md grow-[3] h-full pl-4 z-10 max-md:py-4 max-md:align-middle max-md:w-full"
             id="link_input"
           />
           <button
-            className="bg-cyan py-4 px-7 h-full rounded-md text-white font-bold shrink-0 block z-10"
+            className="bg-cyan py-4 px-7 rounded-md text-white font-bold shrink-0 z-10 grow max-md:w-full"
             onClick={() => shorten()}
           >
             Shorten it!
@@ -102,7 +138,7 @@ export default function Home() {
           </p>
         </div>
         <div className="max-w-7xl m-auto flex max-md:flex-col max-md:gap-20 justify-between mt-20">
-          <div className="bg-white p-7 relative max-w-96 rounded-md">
+          <div className="bg-white p-7 relative max-w-96 rounded-md max-md:m-auto">
             <div className="rounded-full bg-very_dark_violet h-20 w-20 top-[-35px] absolute flex items-center justify-center">
               <Image
                 src="/icon-brand-recognition.svg"
@@ -119,7 +155,7 @@ export default function Home() {
               your content.
             </p>
           </div>
-          <div className="bg-white p-7 relative max-w-96 rounded-md">
+          <div className="bg-white p-7 relative max-w-96 rounded-md max-md:m-auto">
             <div className="rounded-full bg-very_dark_violet h-20 w-20 top-[-35px] absolute flex items-center justify-center">
               <Image
                 src="/icon-detailed-records.svg"
@@ -136,7 +172,7 @@ export default function Home() {
               decisions.
             </p>
           </div>
-          <div className="bg-white p-7 relative max-w-96 rounded-md">
+          <div className="bg-white p-7 relative max-w-96 rounded-md max-md:m-auto">
             <div className="rounded-full bg-very_dark_violet h-20 w-20 top-[-35px] absolute flex items-center justify-center">
               <Image
                 src="/icon-fully-customizable.svg"
@@ -163,16 +199,16 @@ export default function Home() {
           className="absolute top-0 left-0 w-full h-full bg-cover z-0"
         />
         <div className="max-w-7xl m-auto text-center py-20 z-10 relative">
-          <h1 className="font-bold text-4xl text-white">
+          <h1 className="font-bold text-4xl text-white max-md:text-3xl">
             Boost your links today
           </h1>
-          <button className="bg-cyan py-3 px-6 rounded-full text-white font-bold mt-5">
+          <button className="bg-cyan py-3 px-6 rounded-full text-white font-bold mt-5 max-md:px-7 max-md:text-lg">
             Get Started
           </button>
         </div>
       </section>
       <footer className="bg-very_dark_violet p-16 pb-24">
-        <div className="flex justify-between max-w-7xl m-auto max-md:flex-col max-md:gap-10">
+        <div className="flex justify-between max-w-7xl m-auto max-md:flex-col max-md:gap-10 max-md:items-center max-md:text-center">
           <WhiteLogo />
           <div className="flex gap-16 max-md:flex-col">
             <div className="flex flex-col gap-3">
